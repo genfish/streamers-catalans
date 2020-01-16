8ea26fa212d63031e933951a74fd03f539d722fc
genfish <david.buxarrais@genfish.com>
2020-01-16 15:43:44 +0100

    initial commit

---
+++ twitch/viewer.js
@@ -0,0 +1,485 @@
+(function($){$(window).on('load',function(){
+
+var data = {
+	"users" : [
+		{
+			"id"			: ""
+		,	"name"			: "4Xsample"
+		,	"url"			: "4xsample"
+		,	"image"			: "4xsample.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "Abelli97"
+		,	"url"			: "abelli97"
+		,	"image"			: "abelli97.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "Adriats"
+		,	"url"			: "adriats"
+		,	"image"			: "adriats.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "Arandur"
+		,	"url"			: "arandur"
+		,	"image"			: "arandur.jpg"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "Barrowlol"
+		,	"url"			: "barrowlol"
+		,	"image"			: "barrowlol.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "bitrollvr"
+		,	"url"			: "bitrollvr"
+		,	"image"			: "bitrollvr.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "Blaucat"
+		,	"url"			: "blaucat"
+		,	"image"			: "blaucat.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "bruc84"
+		,	"url"			: "bruc84"
+		,	"image"			: "bruc84.jpg"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "Butifarrambseques"
+		,	"url"			: "butifarrambseques"
+		,	"image"			: "butifarrambseques.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "cata_jocs"
+		,	"url"			: "cata_jocs"
+		,	"image"			: "cata_jocs.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "Catalan Company"
+		,	"url"			: "catalancompany"
+		,	"image"			: "catalancompany.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "DevId"
+		,	"url"			: "devidcat"
+		,	"image"			: "devidcat.jpg"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "Devilquest"
+		,	"url"			: "devilquest"
+		,	"image"			: "devilquest.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "Dimobo"
+		,	"url"			: "dimobo"
+		,	"image"			: "dimobo.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "ditomushead"
+		,	"url"			: "ditomushead"
+		,	"image"			: "ditomushead.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "Eneru13"
+		,	"url"			: "eneru13"
+		,	"image"			: "eneru13.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "drfelip"
+		,	"url"			: "drfelip"
+		,	"image"			: "drfelip.jpg"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "Dricxors"
+		,	"url"			: "dricxors"
+		,	"image"			: "dricxors.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "Elizabeth Darko"
+		,	"url"			: "elizabethdarko79"
+		,	"image"			: "elizabethdarko79.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "eqidrell"
+		,	"url"			: "eqidrell"
+		,	"image"			: "eqidrell.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "Família Caricú"
+		,	"url"			: "familiacaricu"
+		,	"image"			: "familiacaricu.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "flaytos"
+		,	"url"			: "flaytos"
+		,	"image"			: "flaytos.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "frikiicat"
+		,	"url"			: "frikiicat"
+		,	"image"			: "frikiicat.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "Fur4nk58"
+		,	"url"			: "fur4nk58"
+		,	"image"			: "fur4nk58.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "GERI8CO"
+		,	"url"			: "g8co"
+		,	"image"			: "geri8co.jpg"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "Gaming Català"
+		,	"url"			: "gaming_catala"
+		,	"image"			: "gaming_catala.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "hirokus"
+		,	"url"			: "hirokus"
+		,	"image"			: "hirokus.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "HQuinto"
+		,	"url"			: "hquinto"
+		,	"image"			: "hquinto.jpg"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "honratt"
+		,	"url"			: "honratt"
+		,	"image"			: "honratt.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "Huesitos"
+		,	"url"			: "huesitos"
+		,	"image"			: "huesitos.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "Joandorino"
+		,	"url"			: "joandorino"
+		,	"image"			: "joandorino.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "Kadurry"
+		,	"url"			: "kadurry"
+		,	"image"			: "kadurry.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "khristoper"
+		,	"url"			: "khristoper"
+		,	"image"			: "khristoper.jpg"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "King_Bruc"
+		,	"url"			: "king_bruc"
+		,	"image"			: "king_bruc.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "kl0Se"
+		,	"url"			: "kl0se"
+		,	"image"			: "kl0se.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "KNT_MeDiiNa"
+		,	"url"			: "knt_mediina"
+		,	"image"			: "knt_mediina.jpg"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "loliconsupport"
+		,	"url"			: "loliconsupport"
+		,	"image"			: "loliconsupport.jpeg"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "Lord Farto"
+		,	"url"			: "lord_farto"
+		,	"image"			: "lord_farto.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "Mcbrolylift"
+		,	"url"			: "mcbrolylift"
+		,	"image"			: "mcbrolylift.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "mironet"
+		,	"url"			: "mironet"
+		,	"image"			: "mironet1.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "NaturxND"
+		,	"url"			: "naturxnd"
+		,	"image"			: "naturxnd.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "NIL66"
+		,	"url"			: "nil66"
+		,	"image"			: "nil66.jpg"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "Nintenhype"
+		,	"url"			: "nintenhypecat"
+		,	"image"			: "nintenhypecat.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "Olidoliva"
+		,	"url"			: "olidolivamedia"
+		,	"image"			: "olidoliva.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "PauichiKun"
+		,	"url"			: "pauichikun"
+		,	"image"			: "pauichikun.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "Pepin Gamers"
+		,	"url"			: "pepin_gamers"
+		,	"image"			: "pepin_gamers.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "Pireta CAT"
+		,	"url"			: "piretacat"
+		,	"image"			: "piretacat.jpg"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "pituhype"
+		,	"url"			: "pituhype"
+		,	"image"			: "pituhype.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "predatorKostas"
+		,	"url"			: "predatorkostas"
+		,	"image"			: "predatorkostas.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "Promover420"
+		,	"url"			: "promover420"
+		,	"image"			: "promover.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "pulgatejana"
+		,	"url"			: "pulgatejana"
+		,	"image"			: "pulgatejana.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "R1EROL"
+		,	"url"			: "r1erol"
+		,	"image"			: "r1erol.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "Reltdeats"
+		,	"url"			: "reltdeats"
+		,	"image"			: "reltdeats.jpg"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "Rik_Ruk"
+		,	"url"			: "rik_ruk"
+		,	"image"			: "rik_ruk.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "rkCAT68"
+		,	"url"			: "rkcat68"
+		,	"image"			: "rkcat68.jpeg"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "Rockstr85"
+		,	"url"			: "rockstr85"
+		,	"image"			: "rockstr85.jpg"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "rogerbl"
+		,	"url"			: "rogerbl"
+		,	"image"			: "rogerbl.jpg"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "sau1073"
+		,	"url"			: "sau1073"
+		,	"image"			: "sau1073.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "sefy13"
+		,	"url"			: "sefy13"
+		,	"image"			: "sefy13.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "shendeluth"
+		,	"url"			: "shendeluth"
+		,	"image"			: "shendeluth.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "SickBoy91"
+		,	"url"			: "sickofyouth"
+		,	"image"			: "sickofyouth.jpg"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "Solxmn"
+		,	"url"			: "solxmn"
+		,	"image"			: "solxmn.jpg"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "Takers26"
+		,	"url"			: "takers26"
+		,	"image"			: "takers26.jpg"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "The Atunero"
+		,	"url"			: "theatunero"
+		,	"image"			: "theatunero.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "TheSysteems"
+		,	"url"			: "thesysteems"
+		,	"image"			: "thesysteems.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "Titafreda"
+		,	"url"			: "titafreda"
+		,	"image"			: "titafreda.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "Twitch En Català"
+		,	"url"			: "twitchencatala"
+		,	"image"			: "twitchencatala.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "user1cat"
+		,	"url"			: "user1cat"
+		,	"image"			: "user1cat.jpeg"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "valvestater65"
+		,	"url"			: "valvestater65"
+		,	"image"			: "valvestater65.jpeg"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "VRCatalans"
+		,	"url"			: "vrcatalans"
+		,	"image"			: "vr_catalans.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "warfocat"
+		,	"url"			: "warfocat"
+		,	"image"			: "warfo.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "wintershowl_mm"
+		,	"url"			: "wintershowl_mm"
+		,	"image"			: "wintershowl_mm.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "xarlio89"
+		,	"url"			: "xarlio89"
+		,	"image"			: "xarlio89.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "Xboxers Catalans"
+		,	"url"			: "xboxerscatalans"
+		,	"image"			: "xboxerscatalans.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "XusLord"
+		,	"url"			: "xuslord"
+		,	"image"			: "xuslord.png"
+		}
+	,	{
+			"id"			: ""
+		,	"name"			: "zoodiustv"
+		,	"url"			: "zoodiustv"
+		,	"image"			: "zoodiustv.png"
+		}
+	]
+,	"num_users" : 76
+};
+
+for (var i in data.users)
+{
+	tr = '<tr class="lb__row"><td class="lb__username"><div class="lb__user-img-wrapper">';
+	tr += '<img src="streamers/' + data.users[i].image + '" class="lb__user-img"></div>';
+	tr += '<div><a href="https://go.twitch.tv/' + data.users[i].url + '" target="_blank" class="night">' + data.users[i].name + '</a></div></td></tr>';
+
+	$('.lb__table tbody').append( $(tr) );
+}
+
+$('.lb__row').each(function(index, el) {
+	if (index > 13)
+		$(el).hide();
+});
+
+$('.lb__see-all').on("click", function(e) {
+	e.preventDefault();
+	$('.lb__row').show();
+	$(e.currentTarget).hide();
+});
+
+})})(jQuery)
